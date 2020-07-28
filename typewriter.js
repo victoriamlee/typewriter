3b097a2d9a160a7fc8b583d15605f8ff3eba9c7e
Victoria Lee <victorialee72@live.com>
2020-07-28 15:53:16 +0000

    Fixed code

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,13 @@
 const sentence = "hello there from lighthouse labs";
 
 
+let count = 0;
+
 for (const char in sentence) {
+  count = char * 50
   setTimeout (() => process.stdout.write(sentence[char]), char * 50)
-  };
+};
+
+
+setTimeout (() => process.stdout.write("\n"), count);
 
